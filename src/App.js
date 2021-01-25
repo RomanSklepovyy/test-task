ee9f322216b12a12213af3ec64806960817d7087
Roman <romansklepovyy1@gmail.com>
2021-01-25 13:50:02 +0200

    add styled component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Button, DatePicker} from 'antd';
+import 'antd/dist/antd.css';
+import styled from 'styled-components';
+
+const RedGreetings = styled.h1`
+  color: red;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello world!</h1>
+        <RedGreetings>Red hello world!</RedGreetings>
+        <Button type="primary">Button</Button>
+        <DatePicker placeholder="select date"/>
     </div>
   );
 }
