3e90f9b975fc15eeaaf897e7b907ea633aa63e8a
roman <romansklepovyy1@gmail.com>
2021-02-04 10:11:15 +0200

    add button link in orders table to create table form

--- src/Components/Orders/Order.js
+++ src/Components/Orders/Order.js
@@ -1,17 +1,34 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Button } from 'antd';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 import OrdersTable from './OrdersTable/OrdersTable';
 import { getOrdersThunk } from '../../redux/orders/orderThunks';
 
+const ButtonContainer = styled.div`
+  margin: 15px 0;
+`;
+
 const Order = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(getOrdersThunk());
   }, []);
 
+  const handleCreateClick = () => {
+    history.push('/orders/create');
+  };
+
   return (
-    <OrdersTable />
+    <>
+      <ButtonContainer>
+        <Button onClick={handleCreateClick} type="primary">Create order</Button>
+      </ButtonContainer>
+      <OrdersTable />
+    </>
   );
 };
 
